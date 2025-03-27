53ac367c5c45868774db237a1464b83ede579063
gl183 <gl183@duke.edu>
2025-03-26 22:28:45 -0400

    connect step cmd and verbose cmd to model

--- app/src/main/java/edu/duke/ece651/group6/factorySimulation/TextView.java
+++ app/src/main/java/edu/duke/ece651/group6/factorySimulation/TextView.java
@@ -25,104 +25,12 @@ public class TextView {
         return this.input.readLine();
     }
 
-    /**
-     * processes the command input by user
-     * request 'ITEM' from 'BUILDING'
-     *  step N
-     *  verbose N
-     *  finish
-     * multiple spaces count as one
-     * no word or only space is invalid => ignore silently
-     * 
-     * @param command is the command that the user input
-     * @return the output displayed to user
-     */
-    public String processCommand(String command) {
-        String cleaned_cmd = cleanCommand(command);
-        String[] cleaned_words = cleaned_cmd.split(" ");
-        if (cleaned_words.length == 1 && cleaned_words[0].equals("")) {
-            // if null silently ignore
-            return null;
-        }
-        switch (cleaned_words[0]) {
-            case "request":
-                // check four '
-                /*
-                 * if nothing after last ', then 4 parts (space previously removed)
-                 * can't tell between
-                 *      request 'a' from 'b'
-                 * and
-                 *      request 'a' from 'b
-                 * append a letter to make sure 5 parts
-                 */
-                String tmp = cleaned_cmd + "a";
-                String[] split_by_quote = tmp.split("'");
-                if ( // 4 ' so 5 parts
-                    !(
-                        // make sure the last element is just what is appended
-                        split_by_quote.length == 5 && split_by_quote[4].equals("a") 
-                    ) || !split_by_quote[2].equals(" from ")
-                ) {
-                    return "Invalid command - Usage: request 'ITEM' from 'BUILDING'";
-                }
-                /*
-                 * what if request '   a' from 'b'
-                 */
-                // make sure item in recipe, building in factory
-                // need interface
-                return cleaned_cmd;
-                // break;
-            case "verbose":
-                // check only two words,second word isdigit
-                if (cleaned_words.length != 2 || !isNonnegativeDigit(cleaned_words[1])) {
-                    return "Invalid command - Usage: verbose <int:verbose-level>";
-                }
-                // check within range
-                // what is the range? 0-2?
-                return cleaned_cmd; // echo the cmd for now
-            case "finish":
-                // check only one word
-                return cleaned_words.length == 1 ?
-                    cleaned_cmd : // echo the cmd for now
-                    "Invalid command - Usage: finish";
-            case "step":
-                // check only two words, second word isdigit
-                if (cleaned_words.length != 2 || !isNonnegativeDigit(cleaned_words[1])) {
-                    return "Invalid command - Usage: step <int:verbose-level>";
-                }
-                return cleaned_cmd;
-            default:
-                return "Invalid command - Please try again";
-        }
-    }
+    
 
     public void displayOutput(String output) {
         // if null silently ignore
         if (null != output) this.out.println(output);
     }
 
-    protected String cleanCommand(String cmd) {
-        // split by space, check first word
-        String[] words = cmd.split(" ");
-        // multiple spaces would result in empty element, remove
-        int word_ct = 0;
-        for (String word : words) {
-            if (!word.equals("")) word_ct += 1;
-        }
-        String[] cleaned_words = new String[word_ct];
-        int ct = 0;
-        for (String word : words) {
-            if (!word.equals("")) cleaned_words[ct++] = word;
-        }
-        return String.join(" ", cleaned_words);
-    }
-
-    protected boolean isNonnegativeDigit(String str) {
-        int len = str.length();
-        if (null == str || len == 0) return false;
-        for (int i = 0; i < len; ++i) {
-            if (!Character.isDigit(str.charAt(i))) return false;
-        }
-        return true;
-    }
+    
 }
